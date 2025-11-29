6e8d0b0f2e3c4b84c1f85b8d3c867a30f8c19014
jero <haolan.wang@juniontech.com>
2025-11-29 15:53:12 +0000

    FEAT : add cmakelists and simple gitgore and main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+
+	// Main app;
+	// if (!app.Start())
+	// {
+	// 	Grm::Exit(-1, "Bot start failed!");
+	// 	return -1;
+	// }
+	// app.Wait();
+	// app.Stop();
+	// Grm::Exit(0, "Normal Exit");
+
+	return 0;
+}
